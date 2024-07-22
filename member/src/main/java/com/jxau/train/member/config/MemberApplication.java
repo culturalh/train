6530f4fb0062c374f28e523dcd6efd144a4a7121
CulturalH <1601331821@qq.com>
2024-07-22 14:06:32 +0800

    修改启动日志，增加/member,修改logback日志

--- member/src/main/java/com/jxau/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jxau/train/member/config/MemberApplication.java
@@ -1,13 +1,25 @@
 package com.jxau.train.member.config;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jxau")
+
 public class MemberApplication {
+    //创建日志对象
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+//        SpringApplication.run(MemberApplication.class, args);
+
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Member地址: \thttp://127.0.0.1{}/member",env.getProperty("server.port"));
     }
 }
