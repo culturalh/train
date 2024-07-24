663ecdb6755bb91d98cfd12b226dd77fe1e7e205
CulturalH <1601331821@qq.com>
2024-07-24 15:39:38 +0800

    使用vuex(store)来保存登录信息

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -75,7 +75,7 @@ export default defineComponent({
           notification.success({ description: '登录成功！' });
           // 登录成功，跳到控台主页
           router.push("/");
-          // store.commit("setMember", data.content);
+          store.commit("setMember", data.content);
         } else {
           notification.error({ description: data.message });
         }
