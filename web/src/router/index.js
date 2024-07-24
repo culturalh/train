37d3c3efb230917f9a92ab911067849a7936c2a0
CulturalH <1601331821@qq.com>
2024-07-24 10:24:47 +0800

    增加控制台页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,23 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
   {
     path: '/login',
     name: 'login-view',
     component: () => import('../views/login.vue')
+  },
+  {
+    path: '/',
+    name: 'main-view',
+    component: () => import('../views/main.vue')
   }
 ]
 
