bb264d06bdf45fe08ced2894c201955010909d96
CulturalH <1601331821@qq.com>
2024-07-26 10:57:32 +0800

    集成dom4j,读取当前的持久层xml文件，得到表名和实体名

--- generator/src/main/java/com/jxau/train/generator/server/ServerTest.java
+++ generator/src/main/java/com/jxau/train/generator/server/ServerTest.java
@@ -2,6 +2,7 @@ package com.jxau.train.generator.server;
 
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -16,6 +17,26 @@ public class ServerTest {
         new File(toPath).mkdirs();
     }
     public static void main(String[] args) throws Exception {
+        //读取到pom.xml文件中的文件名
+        String generatorPath = getGeneratorPath();
+
+        Document document = new SAXReader().read("generator/"+generatorPath);
+        //读取对应的table
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText()+"\t"+domainObjectName.getText());
+
+
+        //        FreemarkerUtil.initConfig("test.ftl");
+//        Map<String, Object> param =new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath+"Test.java", param);
+
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
@@ -23,11 +44,6 @@ public class ServerTest {
         Document document = saxReader.read(new File(pomPath));
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
-
-        //        FreemarkerUtil.initConfig("test.ftl");
-//        Map<String, Object> param =new HashMap<>();
-//        param.put("domain", "Test");
-//        FreemarkerUtil.generator(toPath+"Test.java", param);
-
+        return node.getText();
     }
 }
