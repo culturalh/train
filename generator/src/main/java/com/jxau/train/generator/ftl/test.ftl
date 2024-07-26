1dc52e8b3b40bc186253f80c3c934e0cd713a57c
CulturalH <1601331821@qq.com>
2024-07-26 10:42:23 +0800

    集成dom4j,读取pom.xml,得到当前的持久层xml文件

---
+++ generator/src/main/java/com/jxau/train/generator/ftl/test.ftl
@@ -0,0 +1,6 @@
+package com.jxau.train.generator.test;
+
+public class ${domain} {
+
+    private String name;
+}
---
+++ generator/src/main/java/com/jxau/train/generator/server/ServerTest.java
@@ -0,0 +1,33 @@
+package com.jxau.train.generator.server;
+
+
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerTest {
+    static String toPath = "generator\\src\\main\\java\\com\\jxau\\train\\generator\\test\\";
+    static String pomPath = "generator\\pom.xml";
+    static {
+        new File(toPath).mkdirs();
+    }
+    public static void main(String[] args) throws Exception {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(new File(pomPath));
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+
+        //        FreemarkerUtil.initConfig("test.ftl");
+//        Map<String, Object> param =new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath+"Test.java", param);
+
+    }
+}
