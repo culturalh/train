18fdf2186f0c071eaf86951aff6accb58eb05d61
CulturalH <1601331821@qq.com>
2024-07-27 14:59:14 +0800

    创建admin模块，所有模块都是从web模块复制

---
+++ admin/src/main.js
@@ -0,0 +1,51 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Antd, {notification} from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'
+import * as Icons from '@ant-design/icons-vue'
+import axios from "axios";
+import "../src/assets/js/enums";
+
+// createApp(App).use(store).use(Antd).use(router).mount('#app');
+const app = createApp(App);
+app.use(store).use(Antd).use(router).mount('#app');
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i])
+}
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(config => {
+    const _token = store.state.member.token;
+    if (_token) {
+        config.headers.token = _token;
+        console.log("请求头headers增加token：",_token);
+    }
+    //成功之后的处理
+    // 获取token
+    console.log("请求参数：",config);
+    return config;
+}, error => {
+    //失败之后的处理
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(res => {
+    console.log("响应参数：",res);
+    return res;
+},error => {
+    const  response = error.response;
+    if(response.status === 401){
+        console.log("未登录或者登录超时")
+        store.commit("setMember",{});
+        notification.error({description:"未登录或者登录超时"})
+        router.push("/login");
+    }
+    return Promise.reject(error);
+})
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log("环境",process.env.NODE_ENV);
+console.log("服务端地址",process.env.VUE_APP_SERVER);
---
+++ admin/src/views/main.vue
@@ -0,0 +1,69 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <the-header-vue></the-header-vue>
+    <a-layout>
+    <the-sider-vue></the-sider-vue>
+      <a-layout style="padding: 0 24px 24px">
+
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+         <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+import TheHeaderVue from "@/components/the-header";
+import TheSiderVue from "@/components/the-sider";
+import axios from "axios";
+import {notification} from "ant-design-vue";
+import store from "@/store";
+export default defineComponent({
+  components: {
+    TheSiderVue,
+    TheHeaderVue,
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+  },
+  setup() {
+
+    const count = ref(0);
+
+    axios.get("/member/member/count").then((response) => {
+      let data = response.data;
+      if (data.success) {
+        count.value = data.content;
+      } else {
+        notification.error({ description: data.message });
+      }
+    });
+
+    return {
+      count,
+    };
+  },
+});
+</script>
+<style>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
