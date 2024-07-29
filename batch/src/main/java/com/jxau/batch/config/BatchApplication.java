27a2360d975b306da481e15e52e8d7640b4f204b
CulturalH <1601331821@qq.com>
2024-07-29 10:11:14 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/com/jxau/batch/config/BatchApplication.java
@@ -0,0 +1,25 @@
+package com.jxau.batch.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.jxau")
+@MapperScan("com.jxau.train.business.mapper")
+public class BatchApplication {
+    //创建日志对象
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+    public static void main(String[] args) {
+//        SpringApplication.run(MemberApplication.class, args);
+
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Batch地址: \thttp://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+    }
+}
