99afe78edb01d48d6684810c5a6c1b4e83563ded
CulturalH <1601331821@qq.com>
2024-07-29 10:31:39 +0800

    演示springboot自带的定时任务

---
+++ batch/src/main/java/com/jxau/batch/job/SpringBootTestJob.java
@@ -0,0 +1,20 @@
+package com.jxau.batch.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * springboot自带的定时任务只适合单体应用，不适合集群应用
+ * 没法实时更改任务状态和策略
+ */
+@Component
+@EnableScheduling
+public class SpringBootTestJob {
+
+
+//    @Scheduled(cron = "0/5 * * * * ?")
+    public void test() {
+        System.out.println("test springboot job");
+    }
+}
