e6609da1f8570fad289ddde486f351ab0cee9756
CulturalH <1601331821@qq.com>
2024-07-30 19:24:40 +0800

    集成openfeign，实现服务之间的调用

--- batch/src/main/java/com/jxau/train/batch/controller/testController.java
+++ batch/src/main/java/com/jxau/train/batch/controller/testController.java
@@ -1,12 +1,19 @@
 package com.jxau.train.batch.controller;
 
+import com.jxau.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
+
+    @Resource
+    private BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello(){
+        businessFeign.hello();
         return "hello world!Batch!";
     }
 }
---
+++ batch/src/main/java/com/jxau/train/batch/feign/BusinessFeign.java
@@ -0,0 +1,11 @@
+package com.jxau.train.batch.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "business", url = "http://localhost:8002/business")
+public interface BusinessFeign {
+
+    @GetMapping("/hello")
+    String hello();
+}
